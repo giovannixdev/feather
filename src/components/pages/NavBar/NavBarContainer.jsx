a11d0976409444a50dbe05c52991cf18729a48f1
Trevor Carr <trevor.a.carr@gmail.coom>
2020-12-17 23:11:36 -0500

    working on  NavBar

---
+++ src/components/pages/NavBar/NavBarContainer.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import RenderNavBar from './RenderNavBar';
+
+function NavBarContainer() {
+
+    return(
+        <RenderNavBar />
+    )
+}
+
+export default NavBarContainer;
