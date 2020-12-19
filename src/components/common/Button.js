c911dd2b171ff733ebe624f74940d695b623eca0
Cara Dibdin <admin@MacBook-Pro.local>
2020-12-19 17:44:51 -0500

    create common styled elements to be reused and added reusable button style

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const Button = props => {
-  
+const StyledButton = styled.button`
+  background: #9cadc7;
+  border: 1px solid #eff3f8;
+  box-sizing: border-box;
+  border-radius: 8px;
+  width: 100%;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding: 5px;
+  font-size: 18px;
+  color: whitesmoke;
+`;
+
+function Button(props) {
   return (
-    <button
+    <StyledButton
       onClick={props.handleClick}
       disabled={props.isDisabled}
       className={props.classType || 'primary'}
     >
       {props.buttonText}
-    </button>
+    </StyledButton>
   );
-};
+}
 
 export default Button;
 
