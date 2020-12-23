203edf275248dd42526770f6f604449338120a41
Giovanni Lituma <giovanninet@icloud.com>
2020-12-22 23:11:15 -0500

    change SQL login query and responses from /login & /registration to front end

--- server/server.js
+++ server/server.js
@@ -8,27 +8,32 @@ const userController = require('./controllers/userController');
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.post('/api/login', userController.verifyUser, (req,res) => {
-  console.log("Verify USER");
-  res.status(200).json(res.locals.user);
+app.post('/api/login', userController.verifyUser, (req, res) => {
+  console.log('Verify USER');
+  res.status(200).json(`Welcome ${res.locals.first_name}`);
 });
 
-app.post('/api/register', userController.createAccount, userController.createUser, (req,res) => {
-
-  console.log("IN TEST CREATE USER");
-  res.status(200).json(res.locals.user);
-});
+app.post(
+  '/api/register',
+  userController.createAccount,
+  userController.createUser,
+  (req, res) => {
+    console.log('IN TEST CREATE USER');
+    res.status(200).json('Account created successfully');
+  }
+);
 
 // app.get('/api/getUsers', userController.getUsers, (req,res) => {
 
 //   console.log("GETTING USERS");
 //   res.status(200).json(res.locals.users);
 // });
-
 
 //GLOBAL ERROR CATCH
 app.use((err, req, res, next) => {
