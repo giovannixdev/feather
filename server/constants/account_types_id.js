20d1c0d1dc87d4c938d8efa03315b0e33c4c8cc6
Logan <logan.thies@icloud.com>
2020-12-23 16:19:26 -0800

    update constants for uuid type table ids

--- server/constants/account_types_id.js
+++ server/constants/account_types_id.js
@@ -1,3 +1,11 @@
-export const checking = 0;
-export const savings = 1;
-export const credit = 2;
+//REFACTOR
+
+const checking = '1addb46d-4085-4fe6-b66d-e28cc7f5570d';
+const savings = '971f88e1-2066-43b2-95b1-78ed1ca1c1a9';
+const credit = '9836b8d9-9d93-4907-95fe-080af5c6ec13';
+
+module.exports = {
+  checking,
+  savings,
+  credit,
+};
