43c73e132981d36366765751f74c339350d77c7b
Giovanni Lituma <giovanninet@icloud.com>
2020-12-28 18:58:20 -0500

    add logout functionality

--- src/components/pages/NavBar/RenderNavBar.jsx
+++ src/components/pages/NavBar/RenderNavBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { IconButton, Avatar } from '@material-ui/core';
+import { useAuthDispatch, logout, useAuthState } from '../../../state';
 
 const StyledNavBar = styled.div`
   background: #334f79;
@@ -17,9 +19,27 @@ const StyledLink = styled(Link)`
 `;
 
 function RenderNavBar() {
+  const history = useHistory();
+  const dispatch = useAuthDispatch(); // read dispatch method from context
+  const userDetails = useAuthState(); //read user details from context
+
+  const handleLogout = () => {
+    logout(dispatch); //call the logout action
+    history.push({
+      pathname: '/login',
+    });
+  };
   return (
     <StyledNavBar>
       <StyledLink to="/">Feather</StyledLink>
+      {/* TODO: should be placed next to avatar*/}
+      {/* <p>Welcome {userDetails.user.first_name}</p> */}
+      <IconButton
+        style={{ paddingLeft: 'calc(100vw - 200px)' }}
+        onClick={handleLogout}
+      >
+        <Avatar src="/broken-image.jpg" />
+      </IconButton>
     </StyledNavBar>
   );
 }
