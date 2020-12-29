37132f88fa2771f19c410ae5f183ddc78d73f7eb
Giovanni Lituma <giovanninet@icloud.com>
2020-12-28 19:05:43 -0500

    refactor global error handler

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -26,14 +26,35 @@ userController.createUser = (req, res, next) => {
     '${email}',
     '${user_name}',
     '${password}' 
-  );`;
+  ) RETURNING *;`;
 
   db.query(createUserQueryString)
     .then(response => {
+      const { _id, first_name, user_name } = response.rows[0];
+      res.locals.user = { _id, first_name, user_name };
       return next();
     })
     .catch(err => {
-      return next(err);
+      console.log('Error caught in userController.createUser: ', err);
+
+      if (err.constraint === 'users_user_name_unique') {
+        return next({
+          error_message: { error_message: 'Username already exists' },
+          error: err,
+        });
+      } else if (err.constraint === 'users_email_unique') {
+        return next({
+          error_message: { error_message: 'Email already exists' },
+          error: err,
+        });
+      } else {
+        return next({
+          error_message: {
+            error_message: 'Creating account unsuccessful. See server logs',
+          },
+          error: err,
+        });
+      }
     });
 };
 
