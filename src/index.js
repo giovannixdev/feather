c012c0ba3a3df9836f186637935f96856343e8cb
Giovanni Lituma <giovanninet@icloud.com>
2020-12-30 17:36:20 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import AppRoute from './components/pages/AppRoute';
-import { AuthProvider, useAuthContext } from './state/contexts';
+import { AuthProvider } from './state';
 import { NotFoundPage } from './components/pages/NotFound';
-import { LoadingComponent } from './components/common';
 import { HomePage } from './components/pages/Home';
 import { LoginPage } from './components/pages/Login';
 import { RegisterPage } from './components/pages/Register';
-import { NavBar } from './components/pages/NavBar';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 
 const theme = {
