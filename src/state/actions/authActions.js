0ed5ebcf01334bb1a8ea578bc5106996c3a8a80a
Giovanni Lituma <giovanninet@icloud.com>
2020-12-30 17:43:03 -0500

    refactor and lint

--- src/state/actions/authActions.js
+++ src/state/actions/authActions.js
@@ -19,15 +19,13 @@ export async function loginUser(dispatch, loginPayload) {
       // if (data === null) {
       dispatch({ type: 'LOGIN_SUCCESS', payload: data });
       localStorage.setItem('currentUser', JSON.stringify(data));
-      return data;
     }
 
     // dispatch({ type: 'LOGIN_ERROR', error: data.errors[0] });
-    dispatch({ type: 'LOGIN_ERROR', payload: data });
+    dispatch({ type: 'ERROR', payload: data });
 
-    return data;
   } catch (error) {
-    dispatch({ type: 'LOGIN_ERROR', payload: error });
+    dispatch({ type: 'ERROR', payload: error });
   }
 }
 
