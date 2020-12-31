829a51484cb68ead182061aa63bd9d95e2c85572
Logan <logan.thies@icloud.com>
2020-12-31 14:21:21 -0800

    implemented delete functionality for single and reoccurring transactions

--- src/state/reducers/transactionsReducer.js
+++ src/state/reducers/transactionsReducer.js
@@ -13,6 +13,7 @@ const sortTransactions = transactions => {
 export const TransactionsReducer = (prevState, action) => {
   console.log('action from TransactionsReducer->', action);
   let transactions;
+  let deleteId;
 
   switch (action.type) {
     case 'GET_ALL_TRANSACTIONS':
@@ -21,7 +22,9 @@ export const TransactionsReducer = (prevState, action) => {
         transactions: sortTransactions(action.payload),
         loading: false,
       };
+
     case 'POST_TRANSACTIONS':
+      console.info('action in POST -> ', action);
       transactions = [...prevState.transactions, action.payload.transactions];
 
       return {
@@ -30,12 +33,32 @@ export const TransactionsReducer = (prevState, action) => {
         message: action.payload.message,
         loading: false,
       };
+
+    case 'DELETE_TRANSACTIONS':
+      console.info('action in DELETE -> ', action);
+
+      deleteId = action.payload.reoccurance_id || action.payload.reoccurance_id;
+      transactions = [...prevState.transactions].filter(
+        tr => (tr.reoccurance_id || tr._id) !== deleteId
+      );
+      console.log(
+        'transactions in DELETE_TRANSACTIONS after sort is -> ',
+        transactions
+      );
+      return {
+        ...prevState,
+        transactions: sortTransactions(transactions),
+        message: action.payload.message,
+        loading: false,
+      };
+
     case 'ERROR':
       return {
         ...prevState,
         loading: false,
         error: action.payload.error,
       };
+
     default:
       throw new Error(`Unhandled action type: ${action.type}`);
   }
